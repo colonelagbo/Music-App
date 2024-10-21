0bca7cdc9248e37ea26b81737c597703dfee2124
Emmanuel Agbo <link4emmy@gmail.com>
2024-10-21 12:53:43 +0100

    updated header image and icon

--- music-app/src/components/Header.jsx
+++ music-app/src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Header({ onNavigateHome }) {
   return (
-    <header className="relative h-64 bg-cover bg-center" style={{backgroundImage: "url('/src/images/header-image.jpg')"}}>
+    <header className="relative h-64 bg-cover bg-center" style={{backgroundImage: "url('/src/header-image.jpg')"}}>
       <div className="absolute top-0 right-0 m-4">
       <button onClick={onNavigateHome} className="p-2 rounded-full bg-white shadow-md">
-          <img src="/src/images/home-icon.jpg" alt="Home" className="w-6 h-6" />
+          <img src="/src/home-icon.jpg" alt="Home" className="w-6 h-6" />
         </button>
       </div>
     </header>
